faa509ce0af7929d8e3c89e942bdec6b0b17abc7
y_uuki <yuki.tsubo@gmail.com>
2016-01-26 00:37:27 +0900

    gokc v0.1.0

---
+++ gokc.rb
@@ -0,0 +1,33 @@
+class gokc < Formula
+  gokc_version = '0.1.-'
+  homepage 'https://github.com/yuuki1/gokc'
+  version gokc_version
+
+  if Hardware.is_64_bit?
+    url "https://github.com/yuuki1/gokc/releases/download/v#{gokc_version}/gokc_darwin_amd64.zip"
+    sha256 '343c54dc7bc0c247a7b2d4831341785f1469307f46b1502547dc449a9be23fc0'
+  else
+    url "https://github.com/yuuki1/gokc/releases/download/v#{gokc_version}/gokc_darwin_386.zip"
+    sha256 '9d95d44c01956f5abf36f5079d60eddea5d828681bcd0ac49af00ab1051b046b'
+  end
+
+  head do
+    url 'https://github.com/yuuki1/gokc.git'
+    depends_on 'go' => :build
+    depends_on 'git' => :build
+  end
+
+  def install
+    if build.head?
+      ENV['GOPATH'] = buildpath/'.go'
+      mkdir_p buildpath/'.go/src/github.com/yuuki1'
+      ln_s buildpath, buildpath/'.go/src/github.com/yuuki1/gokc'
+      system 'make', 'build'
+    end
+    bin.install 'gokc'
+  end
+
+  test do
+    system 'gokc', '-v'
+  end
+end
